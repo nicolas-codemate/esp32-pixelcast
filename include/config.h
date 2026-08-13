0b977de2bb98b3fcd5c0a493b67f58e4e8088c9c
Nicolas <nicolas@codemate.consulting>
2026-08-13 19:46:57 +0200

    Update: bump version to 1.9.4

--- include/config.h
+++ include/config.h
@@ -24,10 +24,10 @@
     #define VERSION_MINOR 9
 #endif
 #ifndef VERSION_PATCH
-    #define VERSION_PATCH 3
+    #define VERSION_PATCH 4
 #endif
 #ifndef VERSION_STRING
-    #define VERSION_STRING "1.9.3"
+    #define VERSION_STRING "1.9.4"
 #endif
 
 // ============================================================================

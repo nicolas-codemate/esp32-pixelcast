0bfa678f11f35a45c91dbab247840883b6a6ed6e
Nicolas <nicolas@codemate.consulting>
2026-08-13 19:20:56 +0200

    Docs: document the 502 from POST /api/icons/lametric and bump to 1.9.3

--- include/config.h
+++ include/config.h
@@ -24,10 +24,10 @@
     #define VERSION_MINOR 9
 #endif
 #ifndef VERSION_PATCH
-    #define VERSION_PATCH 2
+    #define VERSION_PATCH 3
 #endif
 #ifndef VERSION_STRING
-    #define VERSION_STRING "1.9.2"
+    #define VERSION_STRING "1.9.3"
 #endif
 
 // ============================================================================
